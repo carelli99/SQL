bfc6c9ba48070460630fc457444f4441a7340afe
carelli99 <alexcarelli99@gmail.com>
2020-05-05 12:44:08 +0200

    Add files via upload

---
+++ test_vincoli.sql
@@ -0,0 +1,7 @@
+use universita;
+insert into corso_laurea(codice, descrizione)
+    values('INF', 'test');
+insert into studente
+    values('-50', 'INF', 'Alessandro', 'Carelli', '1999-02-24', 'test', NULL);
+insert into studente
+    values('10', 'INF', 'Alessandro', 'Carelli', '1999-02-24', 'test2', NULL);
